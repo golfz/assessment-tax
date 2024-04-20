4a306c3f8f052bbbdf2b632a74c3073fe6b723a9
surattikorn <surattikorn@gmail.com>
2024-04-21 01:45:14 +0700

    feat: add custom config case

--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,50 @@
 package config
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func TestNew(t *testing.T) {
+func TestNewWith_Default(t *testing.T) {
+	// Arrange
+	cfgGetter := func(key string) string {
+		return ""
+	}
 
+	// Act
+	cfg := NewWith(cfgGetter)
+
+	// Assert
+	if cfg.Port != defaultPort {
+		t.Errorf("want %d, got %d", defaultPort, cfg.Port)
+	}
+	if cfg.DatabaseURL != defaultDatabaseURL {
+		t.Errorf("want %s, got %s", defaultDatabaseURL, cfg.DatabaseURL)
+	}
+}
+
+func TestNewWith_Custom(t *testing.T) {
+	// Arrange
+	wantPort := 1234
+	wantDatabaseURL := "database-url"
+	cfgGetter := func(key string) string {
+		if key == kPort {
+			return strconv.Itoa(wantPort)
+		}
+		if key == kDatabaseURL {
+			return wantDatabaseURL
+		}
+		return ""
+	}
+
+	// Act
+	cfg := NewWith(cfgGetter)
+
+	// Assert
+	if cfg.Port != wantPort {
+		t.Errorf("want %d, got %d", wantPort, cfg.Port)
+	}
+	if cfg.DatabaseURL != wantDatabaseURL {
+		t.Errorf("want %s, got %s", wantDatabaseURL, cfg.DatabaseURL)
+	}
 }
