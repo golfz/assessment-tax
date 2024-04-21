46f345ff8d9a0488eb4cc96dac5411d38f01000e
surattikorn <surattikorn@gmail.com>
2024-04-21 18:24:14 +0700

    feat: add calculate step 1 & 2

--- tax/tax.go
+++ tax/tax.go
@@ -1,1 +1,59 @@
 package tax
+
+type AllowanceType string
+
+const (
+	AllowanceTypeDonation AllowanceType = "donation"
+	AllowanceTypeKReceipt AllowanceType = "k-receipt"
+)
+
+type Allowance struct {
+	Type   AllowanceType
+	Amount float64
+}
+
+type TaxInformation struct {
+	TotalIncome float64
+	WHT         float64
+	Allowances  []Allowance
+}
+
+type TaxResult struct {
+	Tax float64
+}
+
+func CalculateTax(info TaxInformation) (TaxResult, error) {
+	personalDeduction := 60_000.0
+	taxableIncome := info.TotalIncome - personalDeduction
+
+	// Calculate tax
+	tax := 0.0
+
+	// 0 - 150,000 = 0%
+	left := 0.0
+	right := 150_000.0
+	taxRate := 0.0
+	if taxableIncome > left {
+		incomeForThisRate := taxableIncome - left
+		if incomeForThisRate > right {
+			incomeForThisRate = right - left
+		}
+		tax += incomeForThisRate * (taxRate / 100.0)
+	}
+
+	// 150,001 - 500,000 = 10%
+	left = 150_000.0
+	right = 500_000.0
+	taxRate = 10.00
+	if taxableIncome > left {
+		incomeForThisRate := taxableIncome - left
+		if incomeForThisRate > right {
+			incomeForThisRate = right - left
+		}
+		tax += incomeForThisRate * (taxRate / 100.0)
+	}
+
+	return TaxResult{
+		Tax: tax,
+	}, nil
+}
--- tax/tax_test.go
+++ tax/tax_test.go
@@ -1,1 +1,37 @@
 package tax
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestCalculateTax(t *testing.T) {
+	// Arrange
+	info := TaxInformation{
+		TotalIncome: 500_000.0,
+		WHT:         0.0,
+		Allowances: []Allowance{
+			{
+				Type:   AllowanceTypeDonation,
+				Amount: 0.0,
+			},
+		},
+	}
+	want := TaxResult{
+		Tax: 29_000.0,
+	}
+
+	// Act
+	got, err := CalculateTax(info)
+
+	// Assert
+	fmt.Printf("got: %#v\n", got)
+	if err != nil {
+		t.Errorf("CalculateTax() error = %v", err)
+		return
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("CalculateTax() = %v, want %v", got, want)
+	}
+}
