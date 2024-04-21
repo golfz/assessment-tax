87803fcdb989aaabf20210b8ba977ba6e8d6d3a8
surattikorn <surattikorn@gmail.com>
2024-04-21 20:14:12 +0700

    test: introduce test table

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -8,31 +8,44 @@ import (
 
 func TestCalculateTax_onlyTotalIncome(t *testing.T) {
 	// Arrange
-	info := TaxInformation{
-		TotalIncome: 500_000.0,
-		WHT:         0.0,
-		Allowances: []Allowance{
-			{
-				Type:   AllowanceTypeDonation,
-				Amount: 0.0,
-			},
+	testcases := []struct {
+		name      string
+		info      TaxInformation
+		deduction Deduction
+		want      TaxResult
+	}{
+		{
+			name:      "tax level 1: income=100,000 deduction.personal=0; expect tax=0",
+			info:      TaxInformation{TotalIncome: 100_000.0},
+			deduction: Deduction{Personal: 0.0},
+			want:      TaxResult{Tax: 0.0},
+		},
+		{
+			name:      "tax level 2: income=500,000 deduction.personal=60,000; expect tax=29,000",
+			info:      TaxInformation{TotalIncome: 500_000.0},
+			deduction: Deduction{Personal: 60_000.0},
+			want:      TaxResult{Tax: 29_000.0},
+		},
+		{
+			name:      "tax level 2: income=500,000 deduction.personal=0; expect tax=35,000",
+			info:      TaxInformation{TotalIncome: 500_000.0},
+			deduction: Deduction{Personal: 0.0},
+			want:      TaxResult{Tax: 35_000.0},
 		},
 	}
-	deduction := Deduction{Personal: 60_000.0}
-	want := TaxResult{
-		Tax: 29_000.0,
-	}
 
-	// Act
-	got, err := CalculateTax(info, deduction)
+	for _, tc := range testcases {
+		// Act
+		got, err := CalculateTax(tc.info, tc.deduction)
 
-	// Assert
-	fmt.Printf("got: %#v\n", got)
-	if err != nil {
-		t.Errorf("CalculateTax() error = %v", err)
-		return
-	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("CalculateTax() = %v, want %v", got, want)
+		// Assert
+		fmt.Printf("got: %#v\n", got)
+		if err != nil {
+			t.Errorf("CalculateTax() error = %v", err)
+			return
+		}
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("CalculateTax() = %v, want %v", got, tc.want)
+		}
 	}
 }
