196c00b3a0b1337ce46ad5a678bb0efc46a45128
surattikorn <surattikorn@gmail.com>
2024-04-21 20:47:51 +0700

    feat: add rate 15%

--- tax/tax.go
+++ tax/tax.go
@@ -27,7 +27,7 @@ type Deduction struct {
 }
 
 func CalculateTax(info TaxInformation, deduction Deduction) (TaxResult, error) {
-	taxableIncome := info.TotalIncome - deduction.Personal
+	netIncome := info.TotalIncome - deduction.Personal
 
 	// Calculate tax
 	tax := 0.0
@@ -36,24 +36,39 @@ func CalculateTax(info TaxInformation, deduction Deduction) (TaxResult, error) {
 	left := 0.0
 	right := 150_000.0
 	taxRate := 0.0
-	if taxableIncome > left {
-		incomeForThisRate := taxableIncome - left
-		if incomeForThisRate > right {
-			incomeForThisRate = right - left
+	if netIncome > left {
+		taxableIncome := netIncome - left
+		taxRange := right - left
+		if taxableIncome > taxRange {
+			taxableIncome = right - left
 		}
-		tax += incomeForThisRate * (taxRate / 100.0)
+		tax += taxableIncome * (taxRate / 100.0)
 	}
 
 	// 150,001 - 500,000 = 10%
 	left = 150_000.0
 	right = 500_000.0
 	taxRate = 10.00
-	if taxableIncome > left {
-		incomeForThisRate := taxableIncome - left
-		if incomeForThisRate > right {
-			incomeForThisRate = right - left
+	if netIncome > left {
+		taxableIncome := netIncome - left
+		taxRange := right - left
+		if taxableIncome > taxRange {
+			taxableIncome = right - left
 		}
-		tax += incomeForThisRate * (taxRate / 100.0)
+		tax += taxableIncome * (taxRate / 100.0)
+	}
+
+	// 500,001 - 1,000,000 = 15%
+	left = 500_000.0
+	right = 1_000_000.0
+	taxRate = 15.00
+	if netIncome > left {
+		taxableIncome := netIncome - left
+		taxRange := right - left
+		if taxableIncome > taxRange {
+			taxableIncome = right - left
+		}
+		tax += taxableIncome * (taxRate / 100.0)
 	}
 
 	return TaxResult{
--- tax/tax_test.go
+++ tax/tax_test.go
@@ -1,7 +1,6 @@
 package tax
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
@@ -15,37 +14,56 @@ func TestCalculateTax_onlyTotalIncome(t *testing.T) {
 		want      TaxResult
 	}{
 		{
-			name:      "tax level 1: income=100,000 deduction.personal=0; expect tax=0",
+			name:      "rate 0%: income=100,000 deduction.personal=0; expect tax=0",
 			info:      TaxInformation{TotalIncome: 100_000.0},
 			deduction: Deduction{Personal: 0.0},
 			want:      TaxResult{Tax: 0.0},
 		},
 		{
-			name:      "tax level 2: income=500,000 deduction.personal=60,000; expect tax=29,000",
+			name:      "rate 0%: income=150,000 deduction.personal=0; expect tax=0",
+			info:      TaxInformation{TotalIncome: 150_000.0},
+			deduction: Deduction{Personal: 0.0},
+			want:      TaxResult{Tax: 0.0},
+		},
+		{
+			name:      "rate 10%: income=150,001 deduction.personal=0; expect tax=35,000",
+			info:      TaxInformation{TotalIncome: 150_001.0},
+			deduction: Deduction{Personal: 0.0},
+			want:      TaxResult{Tax: 0.1},
+		},
+		{
+			name:      "rate 10%: income=500,000 deduction.personal=60,000; expect tax=29,000 (EXP01)",
 			info:      TaxInformation{TotalIncome: 500_000.0},
 			deduction: Deduction{Personal: 60_000.0},
 			want:      TaxResult{Tax: 29_000.0},
 		},
 		{
-			name:      "tax level 2: income=500,000 deduction.personal=0; expect tax=35,000",
+			name:      "rate 10%: income=500,000 deduction.personal=0; expect tax=35,000",
 			info:      TaxInformation{TotalIncome: 500_000.0},
 			deduction: Deduction{Personal: 0.0},
 			want:      TaxResult{Tax: 35_000.0},
 		},
+		{
+			name:      "rate 10% + 15%: income=500,001 deduction.personal=0; expect tax=35,000.15",
+			info:      TaxInformation{TotalIncome: 500_001.0},
+			deduction: Deduction{Personal: 0.0},
+			want:      TaxResult{Tax: 35_000.15},
+		},
 	}
 
 	for _, tc := range testcases {
-		// Act
-		got, err := CalculateTax(tc.info, tc.deduction)
+		t.Run(tc.name, func(t *testing.T) {
+			// Act
+			got, err := CalculateTax(tc.info, tc.deduction)
 
-		// Assert
-		fmt.Printf("got: %#v\n", got)
-		if err != nil {
-			t.Errorf("CalculateTax() error = %v", err)
-			return
-		}
-		if !reflect.DeepEqual(got, tc.want) {
-			t.Errorf("CalculateTax() = %v, want %v", got, tc.want)
-		}
+			// Assert
+			if err != nil {
+				t.Errorf("CalculateTax() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("CalculateTax() = %v, want %v", got, tc.want)
+			}
+		})
 	}
 }
