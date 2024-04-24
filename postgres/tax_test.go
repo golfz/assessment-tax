0f231f95a723548c4c7fb55c7449e7d66c448d7a
surattikorn <surattikorn@gmail.com>
2024-04-24 16:06:18 +0700

    test: change success case for TestGetDeduction_Success to table tests

--- postgres/tax_test.go
+++ postgres/tax_test.go
@@ -8,31 +8,73 @@ import (
 )
 
 func TestGetDeduction_Success(t *testing.T) {
-	// Arrange
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-	rows := sqlmock.NewRows([]string{"name", "amount"}).
-		AddRow("personal", "60000.00").
-		AddRow("k-receipt", "50000.00").
-		AddRow("donation", "100000.00").
-		AddRow("unknown", "60000.00")
-	mock.ExpectQuery(`SELECT name, amount FROM deductions`).WillReturnRows(rows)
-	pg := Postgres{Db: db}
-	want := tax.Deduction{
-		Personal: 60000.00,
-		KReceipt: 50000.00,
-		Donation: 100000.00,
+	testcases := []struct {
+		name string
+		rows *sqlmock.Rows
+		want tax.Deduction
+	}{
+		{
+			name: "found all deductions, expect deduction according to rows",
+			rows: sqlmock.NewRows([]string{"name", "amount"}).
+				AddRow("personal", "60000.00").
+				AddRow("k-receipt", "50000.00").
+				AddRow("donation", "100000.00"),
+			want: tax.Deduction{
+				Personal: 60000.00,
+				KReceipt: 50000.00,
+				Donation: 100000.00,
+			},
+		},
+		{
+			name: "not found any deductions, expect deduction with zero value",
+			rows: sqlmock.NewRows([]string{"name", "amount"}),
+			want: tax.Deduction{
+				Personal: 0.0,
+				KReceipt: 0.0,
+				Donation: 0.0,
+			},
+		},
+		{
+			name: "found unknown deductions, expect deduction with zero value",
+			rows: sqlmock.NewRows([]string{"name", "amount"}).
+				AddRow("unknown", "10000.00"),
+			want: tax.Deduction{
+				Personal: 0.0,
+				KReceipt: 0.0,
+				Donation: 0.0,
+			},
+		},
+		{
+			name: "found some deductions, expect deduction according to row found and other with zero value",
+			rows: sqlmock.NewRows([]string{"name", "amount"}).
+				AddRow("k-receipt", "50000.00"),
+			want: tax.Deduction{
+				Personal: 0.0,
+				KReceipt: 50000.00,
+				Donation: 0.0,
+			},
+		},
 	}
 
-	// Act
-	deduction, err := pg.GetDeduction()
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a mock database connection", err)
+			}
+			defer db.Close()
+			mock.ExpectQuery(`SELECT name, amount FROM deductions`).WillReturnRows(tc.rows)
+			pg := Postgres{Db: db}
 
-	// Assert
-	assert.NoError(t, err)
-	assert.Equal(t, want, deduction)
+			// Act
+			deduction, err := pg.GetDeduction()
+
+			// Assert
+			assert.NoError(t, err)
+			assert.Equal(t, tc.want, deduction)
+		})
+	}
 }
 
 func TestGetDeduction_Error(t *testing.T) {
