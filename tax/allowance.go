236f5d7cb722d645819d09533d55389a22d0470c
surattikorn <surattikorn@gmail.com>
2024-04-25 17:32:06 +0700

    feat: change spec of allowance func

--- tax/allowance.go
+++ tax/allowance.go
@@ -1,13 +1,25 @@
 package tax
 
-func collapseAllowance(allowances []Allowance) []Allowance {
-	allowanceSums := make(map[AllowanceType]float64)
+import "math"
+
+func collapseAllowance(allowances []Allowance) map[AllowanceType]float64 {
+	result := make(map[AllowanceType]float64)
 	for _, a := range allowances {
-		allowanceSums[a.Type] += a.Amount // Directly add 'a.Amount' to the map value.
+		result[a.Type] += a.Amount // Directly add 'a.Amount' to the map value.
 	}
-	result := make([]Allowance, 0, len(allowanceSums))
-	for k, v := range allowanceSums {
-		result = append(result, Allowance{Type: k, Amount: v})
+	return result
+}
+
+func getTaxableAllowance(allowances []Allowance, deduction Deduction) map[AllowanceType]float64 {
+	result := collapseAllowance(allowances)
+
+	for aType, aAmount := range result {
+		switch aType {
+		case AllowanceTypeDonation:
+			result[aType] = math.Min(aAmount, deduction.Donation)
+		case AllowanceTypeKReceipt:
+			result[aType] = math.Min(aAmount, deduction.KReceipt)
+		}
 	}
 	return result
 }
--- tax/allowance_test.go
+++ tax/allowance_test.go
@@ -28,7 +28,9 @@ func TestCollapseAllowance_WithSingleAllowance_ExpectSameAllowance(t *testing.T)
 	result := collapseAllowance(allowances)
 
 	// Assert
-	assert.Equal(t, allowances, result)
+	assert.Equal(t, map[AllowanceType]float64{
+		AllowanceTypeDonation: 100_000.0,
+	}, result)
 }
 
 func TestCollapseAllowance_WithMultipleAllowance_ExpectSummedAllowance(t *testing.T) {
@@ -43,8 +45,114 @@ func TestCollapseAllowance_WithMultipleAllowance_ExpectSummedAllowance(t *testin
 	result := collapseAllowance(allowances)
 
 	// Assert
-	assert.Equal(t, []Allowance{
-		{Type: AllowanceTypeDonation, Amount: 150_000.0},
+	assert.Equal(t, map[AllowanceType]float64{
+		AllowanceTypeDonation: 150_000.0,
+		AllowanceTypeKReceipt: 50_000.0,
+	}, result)
+}
+
+func TestGetTaxableAllowance_WithEmptyAllowance_ExpectEmpty(t *testing.T) {
+	// Arrange
+	allowances := []Allowance{}
+	deduction := Deduction{
+		Personal: 60_000.0,
+		KReceipt: 50_000.0,
+		Donation: 100_000.0,
+	}
+
+	// Act
+	result := getTaxableAllowance(allowances, deduction)
+
+	// Assert
+	assert.Empty(t, result)
+}
+
+func TestGetTaxableAllowance_WithSingleAllowance_ExpectSameAllowance(t *testing.T) {
+	// Arrange
+	allowances := []Allowance{
+		{Type: AllowanceTypeDonation, Amount: 80_000.0},
+	}
+	deduction := Deduction{
+		Personal: 60_000.0,
+		KReceipt: 50_000.0,
+		Donation: 100_000.0,
+	}
+
+	// Act
+	result := getTaxableAllowance(allowances, deduction)
+
+	// Assert
+	assert.Equal(t, map[AllowanceType]float64{
+		AllowanceTypeDonation: 80_000.0,
+	}, result)
+}
+
+func TestGetTaxableAllowance_WithMultipleAllowance_ExpectTaxableAllowance(t *testing.T) {
+	// Arrange
+	allowances := []Allowance{
+		{Type: AllowanceTypeDonation, Amount: 30_000.0},
+		{Type: AllowanceTypeDonation, Amount: 40_000.0},
 		{Type: AllowanceTypeKReceipt, Amount: 50_000.0},
+	}
+	deduction := Deduction{
+		Personal: 60_000.0,
+		KReceipt: 50_000.0,
+		Donation: 100_000.0,
+	}
+
+	// Act
+	result := getTaxableAllowance(allowances, deduction)
+
+	// Assert
+	assert.Equal(t, map[AllowanceType]float64{
+		AllowanceTypeDonation: 70_000.0,
+		AllowanceTypeKReceipt: 50_000.0,
+	}, result)
+}
+
+func TestGetTaxableAllowance_WithAllowanceMoreThanDeduction_ExpectSameDeduction(t *testing.T) {
+	// Arrange
+	allowances := []Allowance{
+		{Type: AllowanceTypeDonation, Amount: 130_000.0},
+		{Type: AllowanceTypeDonation, Amount: 70_000.0},
+		{Type: AllowanceTypeKReceipt, Amount: 200_000.0},
+	}
+	deduction := Deduction{
+		Personal: 60_000.0,
+		KReceipt: 50_000.0,
+		Donation: 100_000.0,
+	}
+
+	// Act
+	result := getTaxableAllowance(allowances, deduction)
+
+	// Assert
+	assert.Equal(t, map[AllowanceType]float64{
+		AllowanceTypeDonation: deduction.Donation,
+		AllowanceTypeKReceipt: deduction.KReceipt,
+	}, result)
+}
+
+func TestGetTaxableAllowance_WithAllowanceEqualDeduction_ExpectSameAllowance(t *testing.T) {
+	// Arrange
+	allowances := []Allowance{
+		{Type: AllowanceTypeDonation, Amount: 40_000.0},
+		{Type: AllowanceTypeDonation, Amount: 60_000.0},
+		{Type: AllowanceTypeKReceipt, Amount: 20_000.0},
+		{Type: AllowanceTypeKReceipt, Amount: 30_000.0},
+	}
+	deduction := Deduction{
+		Personal: 60_000.0,
+		KReceipt: 50_000.0,
+		Donation: 100_000.0,
+	}
+
+	// Act
+	result := getTaxableAllowance(allowances, deduction)
+
+	// Assert
+	assert.Equal(t, map[AllowanceType]float64{
+		AllowanceTypeDonation: 100_000.0,
+		AllowanceTypeKReceipt: 50_000.0,
 	}, result)
 }
