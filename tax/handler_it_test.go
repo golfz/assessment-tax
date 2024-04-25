e557ff9d7e640f5812ece03ffd3a5c46d600dedc
surattikorn <surattikorn@gmail.com>
2024-04-25 21:06:58 +0700

    refactor: it-test

--- tax/handler_it_test.go
+++ tax/handler_it_test.go
@@ -17,58 +17,64 @@ import (
 	"testing"
 )
 
-func TestCalculateTaxIntegration_FromIncome_Expect200WithTax(t *testing.T) {
-	t.Run("CalculateTax_Success_Integration", func(t *testing.T) {
-		// Arrange
-		cfg := config.NewWith(os.Getenv)
-
-		pg, err := postgres.New(cfg.DatabaseURL)
-		if err != nil {
-			t.Errorf("failed to connect to database: %v", err)
-		}
-
-		e := echo.New()
-		hTax := tax.New(pg)
-		e.POST("/tax/calculations", hTax.CalculateTaxHandler)
-
-		reqInfoTax := tax.TaxInformation{
-			TotalIncome: 500_000.0,
-			WHT:         0.0,
-			Allowances: []tax.Allowance{
-				{Type: tax.AllowanceTypeDonation, Amount: 0.0},
-			},
-		}
-		var bReader io.Reader
-		b, _ := json.Marshal(reqInfoTax)
-		bReader = strings.NewReader(string(b))
-		req := httptest.NewRequest(http.MethodPost, "/tax/calculations", bReader)
-		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		rec := httptest.NewRecorder()
-
-		wantTax := 29_000.0
-		wantCode := http.StatusOK
-
-		// Act
-		e.ServeHTTP(rec, req)
-
-		// Assert
-		assert.Equal(t, wantCode, rec.Code)
-
-		var got tax.TaxResult
-		if err := json.Unmarshal(rec.Body.Bytes(), &got); err != nil {
-			t.Errorf("expected response body to be valid json, got %s", rec.Body.String())
-		}
-		assert.Equal(t, wantTax, got.Tax)
-	})
-}
-
-func TestCalculateTaxIntegration_FromIncomeAndWHT_Expect200WithTaxAndTaxRefund(t *testing.T) {
+func TestCalculateTaxIntegration_Success_200_OK_With_Tax_And_TaxRefund(t *testing.T) {
 	testcases := []struct {
 		name          string
 		info          tax.TaxInformation
 		wantTax       float64
 		wantTaxRefund float64
 	}{
+		{
+			name: "EXP01: Only TotalIncome; expect tax=29_000.0, taxRefund=0.0",
+			info: tax.TaxInformation{
+				TotalIncome: 500_000.0,
+				WHT:         0.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 0.0},
+				},
+			},
+			wantTax:       29_000.0,
+			wantTaxRefund: 0.0,
+		},
+		{
+			name: "EXP02: Income and WHT; expect tax=4_000.0, taxRefund=0.0",
+			info: tax.TaxInformation{
+				TotalIncome: 500_000.0,
+				WHT:         25_000.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 0.0},
+				},
+			},
+			wantTax:       4_000.0,
+			wantTaxRefund: 0.0,
+		},
+		{
+			name: "EXP02: Income and WHT; expect tax=4_000.0, taxRefund=0.0",
+			info: tax.TaxInformation{
+				TotalIncome: 500_000.0,
+				WHT:         25_000.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 0.0},
+				},
+			},
+			wantTax:       4_000.0,
+			wantTaxRefund: 0.0,
+		},
+		{
+			name: "EXP03: Income and Allowance; expect tax=19_000.0, taxRefund=0.0",
+			info: tax.TaxInformation{
+				TotalIncome: 500_000.0,
+				WHT:         0.0,
+				Allowances: []tax.Allowance{
+					{
+						Type:   tax.AllowanceTypeDonation,
+						Amount: 200_000.0,
+					},
+				},
+			},
+			wantTax:       19_000.0,
+			wantTaxRefund: 0.0,
+		},
 		{
 			name:          "tax > WHT; expect tax>0",
 			info:          tax.TaxInformation{TotalIncome: 500_000.0, WHT: 25_000.0},
@@ -103,15 +109,8 @@ func TestCalculateTaxIntegration_FromIncomeAndWHT_Expect200WithTaxAndTaxRefund(t
 			hTax := tax.New(pg)
 			e.POST("/tax/calculations", hTax.CalculateTaxHandler)
 
-			reqInfoTax := tax.TaxInformation{
-				TotalIncome: tc.info.TotalIncome,
-				WHT:         tc.info.WHT,
-				Allowances: []tax.Allowance{
-					{Type: tax.AllowanceTypeDonation, Amount: 0.0},
-				},
-			}
 			var bReader io.Reader
-			b, _ := json.Marshal(reqInfoTax)
+			b, _ := json.Marshal(tc.info)
 			bReader = strings.NewReader(string(b))
 			req := httptest.NewRequest(http.MethodPost, "/tax/calculations", bReader)
 			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
