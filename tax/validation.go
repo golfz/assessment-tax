489206af62c911ec9d8a8ebf3221868a08c21adb
surattikorn <surattikorn@gmail.com>
2024-04-25 12:26:45 +0700

    refactor: seperate tax/error.go

--- tax/validation.go
+++ tax/validation.go
@@ -12,20 +12,6 @@ const (
 	ConstraintMaxKReceiptDeduction float64 = 100_000.0
 )
 
-var (
-	ErrInvalidDeduction = errors.New("invalid deduction")
-
-	ErrInvalidPersonalDeduction = errors.New("invalid personal deduction")
-	ErrInvalidKReceiptDeduction = errors.New("invalid k-receipt deduction")
-	ErrInvalidDonationDeduction = errors.New("invalid donation deduction")
-
-	ErrInvalidTaxInformation = errors.New("invalid tax information")
-
-	ErrInvalidTotalIncome     = errors.New("total income must be greater than or equal to 0")
-	ErrInvalidWHT             = errors.New("WHT must be greater than or equal to 0 and less than total income")
-	ErrInvalidAllowanceAmount = errors.New("allowance amount must be greater than or equal to 0")
-)
-
 func validateTaxInformation(info TaxInformation) (err error) {
 	if info.TotalIncome < 0 {
 		err = errors.Join(err, ErrInvalidTotalIncome)
