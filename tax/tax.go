6ca75510b59c8a82e8842f72d2e68e39b42c80d5
surattikorn <surattikorn@gmail.com>
2024-04-25 12:20:42 +0700

    refactor: seperate tax/validation.go & its test file

--- tax/tax.go
+++ tax/tax.go
@@ -10,14 +10,6 @@ type AllowanceType string
 const (
 	AllowanceTypeDonation AllowanceType = "donation"
 	AllowanceTypeKReceipt AllowanceType = "k-receipt"
-
-	ConstraintMinPersonalDeduction float64 = 10_000.0
-	ConstraintMaxPersonalDeduction float64 = 100_000.0
-
-	ConstraintMaxDonationDeduction float64 = 100_000.0
-
-	ConstraintMinKReceiptDeduction float64 = 0.0
-	ConstraintMaxKReceiptDeduction float64 = 100_000.0
 )
 
 type Allowance struct {
@@ -54,57 +46,6 @@ var rates = []rate{
 	{lowerBound: 500_000, upperBound: 1_000_000, percentage: 15},
 	{lowerBound: 1_000_000, upperBound: 2_000_000, percentage: 20},
 	{lowerBound: 2_000_000, upperBound: math.MaxFloat64, percentage: 35},
-}
-
-var (
-	ErrInvalidDeduction = errors.New("invalid deduction")
-
-	ErrInvalidPersonalDeduction = errors.New("invalid personal deduction")
-	ErrInvalidKReceiptDeduction = errors.New("invalid k-receipt deduction")
-	ErrInvalidDonationDeduction = errors.New("invalid donation deduction")
-
-	ErrInvalidTaxInformation = errors.New("invalid tax information")
-
-	ErrInvalidTotalIncome     = errors.New("total income must be greater than or equal to 0")
-	ErrInvalidWHT             = errors.New("WHT must be greater than or equal to 0 and less than total income")
-	ErrInvalidAllowanceAmount = errors.New("allowance amount must be greater than or equal to 0")
-)
-
-func validateTaxInformation(info TaxInformation) (err error) {
-	if info.TotalIncome < 0 {
-		err = errors.Join(err, ErrInvalidTotalIncome)
-	}
-
-	if info.WHT < 0 {
-		err = errors.Join(err, ErrInvalidWHT)
-	}
-
-	if info.TotalIncome > 0 && info.WHT > info.TotalIncome {
-		err = errors.Join(err, ErrInvalidWHT)
-	}
-
-	for _, allowance := range info.Allowances {
-		if allowance.Amount < 0 {
-			err = errors.Join(err, ErrInvalidAllowanceAmount)
-		}
-	}
-
-	return
-}
-
-func validateDeduction(deduction Deduction) (err error) {
-	if deduction.Personal <= ConstraintMinPersonalDeduction || deduction.Personal > ConstraintMaxPersonalDeduction {
-		err = errors.Join(err, ErrInvalidPersonalDeduction)
-	}
-
-	if deduction.KReceipt <= ConstraintMinKReceiptDeduction || deduction.KReceipt > ConstraintMaxKReceiptDeduction {
-		err = errors.Join(err, ErrInvalidKReceiptDeduction)
-	}
-
-	if deduction.Donation > ConstraintMaxDonationDeduction {
-		err = errors.Join(err, ErrInvalidDonationDeduction)
-	}
-	return
 }
 
 func calculateTaxableIncome(netIncome, lowerBound, upperBound float64) float64 {
