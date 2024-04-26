3fda3f3f57b4d9a8c8f18c43de1ae1483cb9d232
surattikorn <surattikorn@gmail.com>
2024-04-26 19:17:52 +0700

    test: add postgres/admin_test.go

--- postgres/admin_test.go
+++ postgres/admin_test.go
@@ -1,1 +1,48 @@
+//go:build unit
+
 package postgres
+
+import (
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSetPersonalDeduction_Success(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec("^UPDATE (.+)").WillReturnResult(sqlmock.NewResult(0, 1))
+	pg := Postgres{Db: db}
+
+	// Act
+	err = pg.SetPersonalDeduction(60000.00)
+
+	// Assert
+	assert.NoError(t, err)
+	_ = mock.ExpectationsWereMet()
+}
+
+func TestSetPersonalDeduction_Error(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec("^UPDATE (.+)").WillReturnError(errors.New("unexpected error"))
+	pg := Postgres{Db: db}
+
+	// Act
+	err = pg.SetPersonalDeduction(60000.00)
+
+	// Assert
+	assert.Error(t, err)
+	_ = mock.ExpectationsWereMet()
+}
