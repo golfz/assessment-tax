12bed6f4068058e9f6a3f2005d75677bba42ba4d
surattikorn <surattikorn@gmail.com>
2024-04-26 13:03:17 +0700

    tes addt: TestCalculateTaxHandler_WithTaxLevel_Success in tax/handler_it_test.go

--- tax/handler_it_test.go
+++ tax/handler_it_test.go
@@ -17,7 +17,7 @@ import (
 	"testing"
 )
 
-func TestCalculateTaxIntegration_Success_200_OK_With_Tax_And_TaxRefund(t *testing.T) {
+func TestCalculateTaxIntegration_Success(t *testing.T) {
 	testcases := []struct {
 		name          string
 		taxInfo       tax.TaxInformation
@@ -164,8 +164,107 @@ func TestCalculateTaxIntegration_Success_200_OK_With_Tax_And_TaxRefund(t *testin
 			if err := json.Unmarshal(rec.Body.Bytes(), &gotTaxResult); err != nil {
 				t.Errorf("expected response body to be valid json, got %s", rec.Body.String())
 			}
-			assert.Equal(t, tc.wantTaxResult, gotTaxResult)
+			assert.Equal(t, tc.wantTaxResult.Tax, gotTaxResult.Tax)
+			assert.Equal(t, tc.wantTaxResult.TaxRefund, gotTaxResult.TaxRefund)
 		})
 	}
+}
 
+func TestCalculateTaxHandler_WithTaxLevel_Success(t *testing.T) {
+	// Arrange
+	testcases := []struct {
+		name          string
+		taxInfo       tax.TaxInformation
+		wantTaxResult tax.TaxResult
+		wantTaxLevels []float64
+	}{
+		{
+			name: "EXP04: net-income=340,000 (rate=10%); expect tax=19,000",
+			taxInfo: tax.TaxInformation{
+				TotalIncome: 500_000.0,
+				WHT:         0.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 200000.0},
+				},
+			},
+			wantTaxResult: tax.TaxResult{Tax: 19_000.0, TaxRefund: 0.0},
+			wantTaxLevels: []float64{0.0, 19_000.0, 0.0, 0.0, 0.0},
+		},
+		{
+			name: "net-income=100,000 (rate=0%); expect tax=0",
+			taxInfo: tax.TaxInformation{
+				TotalIncome: 260_000.0,
+				WHT:         0.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 200000.0},
+				},
+			},
+			wantTaxResult: tax.TaxResult{Tax: 0.0, TaxRefund: 0.0},
+			wantTaxLevels: []float64{0.0, 0.0, 0.0, 0.0, 0.0},
+		},
+		{
+			name: "net-income=3,000,000 (rate=35%); expect tax=660,000",
+			taxInfo: tax.TaxInformation{
+				TotalIncome: 3_160_000.0,
+				WHT:         0.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 200000.0},
+				},
+			},
+			wantTaxResult: tax.TaxResult{Tax: 660_000.0, TaxRefund: 0.0},
+			wantTaxLevels: []float64{0.0, 35_000.0, 75_000.0, 200_000.0, 350_000.0},
+		},
+		{
+			name: "net-income=3,000,000 (rate=35%) wht=700,000; expect taxRefund=40,000",
+			taxInfo: tax.TaxInformation{
+				TotalIncome: 3_160_000.0,
+				WHT:         700_000.0,
+				Allowances: []tax.Allowance{
+					{Type: tax.AllowanceTypeDonation, Amount: 200000.0},
+				},
+			},
+			wantTaxResult: tax.TaxResult{Tax: 0.0, TaxRefund: 40_000.0},
+			wantTaxLevels: []float64{0.0, 35_000.0, 75_000.0, 200_000.0, 350_000.0},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			cfg := config.NewWith(os.Getenv)
+
+			pg, err := postgres.New(cfg.DatabaseURL)
+			if err != nil {
+				t.Errorf("failed to connect to database: %v", err)
+			}
+
+			e := echo.New()
+			hTax := tax.New(pg)
+			e.POST("/tax/calculations", hTax.CalculateTaxHandler)
+
+			var bReader io.Reader
+			b, _ := json.Marshal(tc.taxInfo)
+			bReader = strings.NewReader(string(b))
+			req := httptest.NewRequest(http.MethodPost, "/tax/calculations", bReader)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+
+			// Act
+			e.ServeHTTP(rec, req)
+
+			// Assert
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+			var gotTaxResult tax.TaxResult
+			if err := json.Unmarshal(rec.Body.Bytes(), &gotTaxResult); err != nil {
+				t.Errorf("expected response body to be valid json, got %s", rec.Body.String())
+			}
+			assert.Equal(t, tc.wantTaxResult.Tax, gotTaxResult.Tax)
+			assert.Equal(t, tc.wantTaxResult.TaxRefund, gotTaxResult.TaxRefund)
+
+			for i, wantTax := range tc.wantTaxLevels {
+				assert.Equal(t, wantTax, gotTaxResult.TaxLevels[i].Tax)
+			}
+		})
+	}
 }
