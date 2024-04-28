97ba6ac8499634a553c2681540e99751074989cb
surattikorn <surattikorn@gmail.com>
2024-04-28 15:58:34 +0700

    refactor: extract method in postgres/admin.go

--- postgres/admin.go
+++ postgres/admin.go
@@ -1,21 +1,18 @@
 package postgres
 
-func (p *Postgres) SetPersonalDeduction(amount float64) error {
-	updateSql := `UPDATE deductions SET amount = $1 WHERE name = 'personal'`
-	_, err := p.DB.Exec(updateSql, amount)
+func (p *Postgres) setDeduction(deductionType string, amount float64) error {
+	updateSql := "UPDATE deductions SET amount = $1 WHERE name = $2"
+	_, err := p.DB.Exec(updateSql, amount, deductionType)
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func (p *Postgres) SetKReceiptDeduction(amount float64) error {
-	updateSql := `UPDATE deductions SET amount = $1 WHERE name = 'k-receipt'`
-	_, err := p.DB.Exec(updateSql, amount)
-	if err != nil {
-		return err
-	}
+func (p *Postgres) SetPersonalDeduction(amount float64) error {
+	return p.setDeduction("personal", amount)
+}
 
-	return nil
+func (p *Postgres) SetKReceiptDeduction(amount float64) error {
+	return p.setDeduction("k-receipt", amount)
 }
