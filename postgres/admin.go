3d1099dc6aa353fddf756f9b646dc8055b396dcf
surattikorn <surattikorn@gmail.com>
2024-04-28 16:04:22 +0700

    refactor: introduce deductionType instead of string

--- postgres/admin.go
+++ postgres/admin.go
@@ -1,5 +1,10 @@
 package postgres
 
+type deductionType string
+
+const personalDeduction = "personal"
+const kReceiptDeduction = "k-receipt"
+
 func (p *Postgres) setDeduction(deductionType string, amount float64) error {
 	updateSql := "UPDATE deductions SET amount = $1 WHERE name = $2"
 	_, err := p.DB.Exec(updateSql, amount, deductionType)
@@ -10,9 +15,9 @@ func (p *Postgres) setDeduction(deductionType string, amount float64) error {
 }
 
 func (p *Postgres) SetPersonalDeduction(amount float64) error {
-	return p.setDeduction("personal", amount)
+	return p.setDeduction(personalDeduction, amount)
 }
 
 func (p *Postgres) SetKReceiptDeduction(amount float64) error {
-	return p.setDeduction("k-receipt", amount)
+	return p.setDeduction(kReceiptDeduction, amount)
 }
